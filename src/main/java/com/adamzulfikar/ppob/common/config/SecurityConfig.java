e91cefd24a8eff729575de56cfa2bb4c9eb905f7
Adam Zulfikar <adam.zulfikar@nexsoft.co.id>
2025-11-09 01:54:03 +0700

    allow all api

--- src/main/java/com/adamzulfikar/ppob/common/config/SecurityConfig.java
+++ src/main/java/com/adamzulfikar/ppob/common/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.adamzulfikar.ppob.common.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
 
